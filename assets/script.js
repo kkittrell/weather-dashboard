cb00487523643129eeead4454b61d1d5c9fcd990
Kellen Kittrell <kellenkittrell@gmail.com>
2022-10-25 19:02:43 -0700

    added updated js file

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,24 @@
 var apiKey = '84cf894a7e4f7788f5202071b862063a';
+var forecast= document.getElementById("forecast");
+var search = document.getElementById("search");
+var searchCity = document.getElementById("search-city");
+search.addEventListener("click", (event)=> {
+    console.log(searchCity.value); fetch("http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=" + apiKey)
+
+})
+fetch("https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=0&lon=0&appid=" + apiKey)
+
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+    for(i = 0; i < data.list.length; i += 8){
+        console.log(data.list[i])
+        var forecastDay = document.createElement("div")
+        forecastDay.innerHTML = `
+        <p>${data.list[i].main.temp}</p>
+        `
+forecast.append(forecastDay)
+console.log(forecast)
+
+}
+})
